e6e3d887419c2406a4a0734c64d0daea6248843f
VictorChh <71901322+VictorChh@users.noreply.github.com>
2021-03-24 09:58:35 -0600

    Update database.php

--- database.php
+++ database.php
@@ -1,9 +1,9 @@
 <?php
     $connection = mysqli_connect(
-        'localhost',
-        'root',
-        'root',
-        'TaskApp'
+        '70.40.220.123',
+        'alicisiv_4d2',
+        'CEFB5AD32tn4i6a1pe8cy0v7',
+        'alicisiv_4d2'
     );
 
     if($connection){
