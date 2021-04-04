eb3f3eb6fb6bd57437f23a8a3c41cc61c5ced3a3
VictorChh <71901322+VictorChh@users.noreply.github.com>
2021-04-04 11:38:35 -0500

    Update database.php

--- database.php
+++ database.php
@@ -1,20 +1,40 @@
 <?php
+/*
     $connection = mysqli_connect(
-        '70.40.220.123',
-        'alicisiv_4d2',
-        'CEFB5AD32tn4i6a1pe8cy0v7',
-        'alicisiv_4d2'
+        'localhost',
+        'root',
+        'root',
+        'TaskApp'
     );
 
     if($connection){
-        echo "Database connected";
+       // echo "Database connected";
     }
-else{
-    echo "no connection";
-}
+*/
+    //Get Heroku ClearDB connection informati
+    /*
+$cleardb_url = parse_url(getenv("mysql://b0dca37501b2bc:9ba0a5ad@us-cdbr-east-03.cleardb.com/heroku_4849162b69c60d6?reconnect=true"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
 
-$ip_server = $_SERVER['SERVER_ADDR'];
-  
-// Printing the stored address
-echo "Server IP Address is: $ip_server";
+$connection = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);*/
+
+
+    $connection = mysqli_connect(
+        'us-cdbr-east-03.cleardb.com',
+        'b0dca37501b2bc',
+        '9ba0a5ad',
+        'heroku_4849162b69c60d6'
+    );
+
+    if($connection){
+       //echo "Database connected";
+    }
+    
+
 ?>
